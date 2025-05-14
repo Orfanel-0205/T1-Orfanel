d0207eec91b6acd0a654e198c447fa08c9abfbce
Orfanel-0205 <202211750@fit.edu.ph>
2025-05-15 03:48:55 +0800

    feat: added submission form

--- app/Views/welcome_form.php
+++ app/Views/welcome_form.php
@@ -7,7 +7,7 @@
 <body>
     <h2>Enter your name</h2>
     <form  method="post" action="/welcome/greet" >
-        <input type="text" name="name" placeholder="Enter your name">
+        <input type="text" name="name" required>
       <buttton type="submit">Submit</button>
     </form>
     
