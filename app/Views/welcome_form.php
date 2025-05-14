c0152472378ccb2025e0d3cff902fbb5b9c85ec8
Orfanel-0205 <202211750@fit.edu.ph>
2025-05-15 03:37:27 +0800

    feat: added submit feature in ci4

---
+++ app/Views/welcome_form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Clifford Psalm Orfanel</title>
+</head>
+<body>
+    <h2>Enter your name</h2>
+    <form  method="post" action="/welcome/greet" >
+        <input type="text" name="name" placeholder="Enter your name">
+      <buttton type="submit">Submit</button>
+    </form>
+    
+</body>
+</html>
