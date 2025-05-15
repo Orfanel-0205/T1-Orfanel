4600fea28ab5079d8bffda4d411d1c0ee2c2077d
Orfanel-0205 <202211750@fit.edu.ph>
2025-05-15 22:27:16 +0800

    refactor: htaccess codes and css path

--- app/Views/welcome_form.php
+++ app/Views/welcome_form.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
     <title>Clifford Psalm Orfanel</title>
-    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+
+
 </head>
 <body>
     <h2>Enter your name</h2>
