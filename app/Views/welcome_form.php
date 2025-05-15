e21057321c9d1a8820e665f2044d282e52c93bae
Orfanel-0205 <202211750@fit.edu.ph>
2025-05-15 21:54:21 +0800

    refactor: fixed button

--- app/Views/welcome_form.php
+++ app/Views/welcome_form.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
     <title>Clifford Psalm Orfanel</title>
 </head>
 <body>
     <h2>Enter your name</h2>
-    <form  method="post" action="/welcome/greet" >
+    <form method="post" action="/welcome/greet">
         <input type="text" name="name" required>
-      <buttton type="submit">Submit</button>
+        <button type="submit">Submit</button>
     </form>
-    
 </body>
 </html>
